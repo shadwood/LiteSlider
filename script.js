cedd65301df6e89ee6d43925e422374f36feac38
shadwood <38562621+shadwood@users.noreply.github.com>
2020-04-09 19:16:22 +0300

    Add possibility to create several gallery on one page though one function

--- script.js
+++ script.js
@@ -1,77 +1,80 @@
-var currentImgCounter = 0;
-var galleryBodyWidth ;
-var wrap;
+function getGallery(selector) {
 
-function gallery() {
-    var imgs = document.querySelectorAll(".wrap img");
-    var galleryBody = document.getElementsByClassName("gallery-body")[0];
-    var galleryBodyWidth = galleryBody.clientWidth;
-    wrap = document.getElementsByClassName("wrap")[0];
-    imgs.forEach(function(img){
-        var imgWidth = img.clientWidth;
-        var margin = (galleryBodyWidth - imgWidth) /2;
-        img.style.marginLeft=margin+"px";
-        img.style.marginRight=margin+"px";
-    });
+    var currentImgCounter = 0;
+    var galleryBodyWidth;
+    var wrap;
 
-    var rButton = document.getElementById("r-button");
-    var lButton = document.getElementById("l-button");
+    function gallery() {
+        var imgs = document.querySelectorAll("#"+selector +" .wrap img");
+        var galleryBody = document.querySelector("#"+selector +" .gallery-body");
+        var galleryBodyWidth = galleryBody.clientWidth;
+        wrap = document.querySelector("#"+selector +" .wrap");
+        imgs.forEach(function (img) {
+            var imgWidth = img.clientWidth;
+            var margin = (galleryBodyWidth - imgWidth) / 2;
+            img.style.marginLeft = margin + "px";
+            img.style.marginRight = margin + "px";
+        });
+
+        var rButton = document.querySelector("#" + selector + " .r-button");
+        var lButton = document.querySelector("#" + selector + " .l-button");
 
 
-    wrap.style.width = galleryBodyWidth * imgs.length +"px";
+        wrap.style.width = galleryBodyWidth * imgs.length + "px";
 
 
-    function parseTranslateValue(string){
-        var value="";
-        string.split("").forEach(function(char){if(parseInt(char)||parseInt(char)===0)value+=char});
-        return parseInt(value);
+        function parseTranslateValue(string) {
+            var value = "";
+            string.split("").forEach(function (char) {
+                if (parseInt(char) || parseInt(char) === 0) value += char
+            });
+            return parseInt(value);
+        }
+
+        lButton.onclick = function () {
+            wrap.style.transition = "transform 1000ms linear 0s ";
+            var transformValue = wrap.style.transform;
+            var translateValue = transformValue ? parseTranslateValue(transformValue) : 0;
+            if (translateValue > 0) {
+                translateValue -= galleryBodyWidth;
+                wrap.style.transform = "translateX(-" + translateValue + "px)";
+                currentImgCounter--;
+            } else {
+                translateValue = galleryBodyWidth * imgs.length - galleryBodyWidth;
+                wrap.style.transform = "translateX(-" + translateValue + "px)";
+                currentImgCounter = imgs.length - 1;
+            }
+        };
+
+
+        rButton.onclick = function () {
+            wrap.style.transition = "transform 1000ms linear 0s ";
+            var transformValue = wrap.style.transform;
+            var translateValue = transformValue ? parseTranslateValue(transformValue) : 0;
+            if (translateValue < parseInt(wrap.style.width) - galleryBodyWidth) {
+                translateValue += galleryBodyWidth;
+                wrap.style.transform = "translateX(-" + translateValue + "px)";
+                currentImgCounter++;
+            } else {
+                translateValue = 0;
+                wrap.style.transform = "translateX(-" + translateValue + "px)";
+                currentImgCounter = 0;
+            }
+        };
+        return {'currentImgCounter' : currentImgCounter, 'galleryBodyWidth' : galleryBodyWidth}
     }
 
-    lButton.onclick=function(){
-        wrap.style.transition="transform 1000ms linear 0s ";
-        var transformValue = wrap.style.transform;
-        var translateValue = transformValue ? parseTranslateValue(transformValue) : 0;
-        if (translateValue>0){
-            translateValue-=galleryBodyWidth;
-            wrap.style.transform = "translateX(-"+translateValue+"px)";
-            currentImgCounter--;
-        }
-        else {
-            translateValue = galleryBodyWidth * imgs.length - galleryBodyWidth ;
-            wrap.style.transform = "translateX(-"+translateValue+"px)";
-            currentImgCounter=imgs.length-1;
-        }
-    };
+    function resize() {
+        wrap.style.transition = "transform 0ms linear 0s ";
+        let gal = gallery();
+        wrap.style.transform = "translateX(-" + (gal.currentImgCounter * gal.galleryBodyWidth) + "px)";
+    }
 
-
-    rButton.onclick=function(){
-        wrap.style.transition="transform 1000ms linear 0s ";
-        var transformValue = wrap.style.transform;
-        var translateValue = transformValue ? parseTranslateValue(transformValue) : 0;
-        if (translateValue< parseInt(wrap.style.width) - galleryBodyWidth){
-            translateValue+=galleryBodyWidth;
-            wrap.style.transform = "translateX(-"+translateValue+"px)";
-            currentImgCounter++;
-        }
-        else {
-            translateValue = 0;
-            wrap.style.transform = "translateX(-"+translateValue+"px)";
-            currentImgCounter = 0;
-        }
-    };
+    window.addEventListener("load", gallery);
+    window.addEventListener("resize", resize);
 
 }
 
-function resize() {
-    wrap.style.transition="transform 0ms linear 0s ";
-    gallery();
-    wrap.style.transform = "translateX(-"+ (currentImgCounter * galleryBodyWidth )+"px)";
-}
+getGallery("gallery");
 
-window.onload= gallery;
-window.onresize = resize;
-
-
-
-
-
+/*getGallery("gallery2");*/
